fff6e5724761a58e0c8ab7e6d8d23f1e1ae2cd60
Jack-TheDev <143633265+Jack-TheDev@users.noreply.github.com>
2024-03-13 18:19:49 +0000

    Added Page

---
+++ current_projects.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Current Projects</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    
+    <h1>Current Projects</h1>
+    <p>Hampshire RP -- A Roblox RP Game Set in Hampshire, England.  It is Currently in development.</p>
+    
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>

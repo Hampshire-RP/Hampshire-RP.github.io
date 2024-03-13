09f1259e60007dd38da0191c7b3c3013f3b0d622
Jack-TheDev <143633265+Jack-TheDev@users.noreply.github.com>
2024-03-13 18:30:21 +0000

    Add Header

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hampshire RP</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="current_projects.php">Current Projects</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                 <li><a href="previous_projects.php">Previous Projects</a></li>
+                  <li><a href="active_staff.php">Active Staff</a></li>
+            </ul>
+        </nav>
+    </header>

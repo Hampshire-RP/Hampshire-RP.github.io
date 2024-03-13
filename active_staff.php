935dba5040b148f99214f3b61226e0cc5e9e42c0
Jack-TheDev <143633265+Jack-TheDev@users.noreply.github.com>
2024-03-13 18:18:47 +0000

    Adding Page

---
+++ active_staff.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Active Staff</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    
+    <h1>Active Staff</h1>
+    <p>JWW.8663 | JWW -- Founder and Co-owner
+    Spencer -- Founder and Owner
+    
+    --MORE STAFF BEING ADDED VERY SOON--.</p>
+    
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>

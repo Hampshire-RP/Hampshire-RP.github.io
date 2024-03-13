1a9c3d73d5ec64a0c9f33263f1ebbbf55ec48c5f
Jack-TheDev <143633265+Jack-TheDev@users.noreply.github.com>
2024-03-13 18:21:17 +0000

    Added Page

---
+++ previous_projects.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Previous Projects</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    
+    <h1>Previous Projects</h1>
+    <p>N/A         We Currently Don't Have Any Previously Completed Projects.</p>
+    
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>

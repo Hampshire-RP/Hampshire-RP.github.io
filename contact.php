fa62e9a7e20e19bdb5748f6ff21d9d487c5c9682
Jack-TheDev <143633265+Jack-TheDev@users.noreply.github.com>
2024-03-13 18:19:19 +0000

    Added Page

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    
+    <h1>Contact Us</h1>
+    <p>Roblox Group -- roblox.com/groups/9670467
+    
+    Discord Server -- discord.gg/VM2M3upJVz</p>
+    
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>

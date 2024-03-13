8fdf4cf6cc38cabfabdeebeafead6a82ae52e318
Jack-TheDev <143633265+Jack-TheDev@users.noreply.github.com>
2024-03-13 18:20:28 +0000

    Added Page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Roblox Group - Hampshire RP</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    
+    <h1>Welcome to Hampshire RP!</h1>
+    <p>This website showcases our current and previous works and all of our amazing staff team.</p>
+    
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
